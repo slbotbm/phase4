84aca07c83a75f19806b5499c95e112ea41e37bb
skenta114 <133939936+skenta114@users.noreply.github.com>
2023-12-16 16:31:21 +0900

    遷移するページを追加

--- resources/views/looking.blade.php
+++ resources/views/looking.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('HOME') }}
+            {{ __('人を探す') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <b>人を探す機能を追加</b>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\TechnologyController;
-use App\Http\Controllers\ProjectController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -22,12 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('auth')->group(function() {
-    Route::resource('employee', EmployeeController::class);
-    Route::resource('technology', TechnologyController::class);
-    Route::resource('project', ProjectController::class);
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -37,5 +27,14 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/looking', function () {
+    return view('looking');
+})->name('looking.page');
+
+Route::get('/change', function () {
+    return view('change');
+})->name('change.page');
+
 
 require __DIR__.'/auth.php';
